70d25b97b67b2842c722f97a1cd2b925414674e6
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-01-20 20:18:26 -0800

    Rereinit react native with cli

---
+++ frontend/wyai/App.tsx
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  Header,
+  LearnMoreLinks,
+} from 'react-native/Libraries/NewAppScreen';
+import Button from './components/Button';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <Header />
+          <Button />
+        </ScrollView>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+export default App;
