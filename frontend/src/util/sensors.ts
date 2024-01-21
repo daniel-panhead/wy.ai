23006cc0f2c512254297d4466f5215e7118e345f
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-01-21 10:28:21 -0800

    put everything back

--- frontend/src/util/sensors.ts
+++ frontend/src/util/sensors.ts
@@ -81,57 +81,57 @@ export const useAccel = () => {
 
 export const useAhrs = () => {
   const permsGranted = useRef(false)
-  // const [eulerAngles, setEulerAngles] = useState<{ heading: number; pitch: number; roll: number; }>()
-  // const [madgwick, setMadgwick] = useState<AHRS>()
-  // const [gData, setGData] = useState<[number, number, number] | []>([])
-  const [aData, setAData] = useState({x: 0, y: 0, z: 0})
-  // const [mData, setMData] = useState<[number, number, number] | []>([])
+  const [eulerAngles, setEulerAngles] = useState<{ heading: number; pitch: number; roll: number; }>()
+  const [madgwick, setMadgwick] = useState<AHRS>()
+  const [gData, setGData] = useState<[number, number, number] | []>([])
+  const [aData, setAData] = useState<[number, number, number] | []>([])
+  const [mData, setMData] = useState<[number, number, number] | []>([])
 
   useEffect(() => {
     if (!permsGranted.current) {
       getAccelPermission().then((res) => {
         if (res.state === "granted") {
           permsGranted.current = true
-          // const madgwick = new AHRS({
-          //   /*
-          //   * The sample interval, in Hz.
-          //   *
-          //   * Default: 20
-          //   */
-          //   sampleInterval: 10,
+          const madgwick = new AHRS({
+            /*
+            * The sample interval, in Hz.
+            *
+            * Default: 20
+            */
+            sampleInterval: 10,
 
-          //   /*
-          //   * Choose from the `Madgwick` or `Mahony` filter.
-          //   *
-          //   * Default: 'Madgwick'
-          //   */
-          //   algorithm: 'Madgwick',
+            /*
+            * Choose from the `Madgwick` or `Mahony` filter.
+            *
+            * Default: 'Madgwick'
+            */
+            algorithm: 'Madgwick',
 
-          //   /*
-          //   * The filter noise value, smaller values have
-          //   * smoother estimates, but have higher latency.
-          //   * This only works for the `Madgwick` filter.
-          //   *
-          //   * Default: 0.4
-          //   */
-          //   beta: 0.4,
+            /*
+            * The filter noise value, smaller values have
+            * smoother estimates, but have higher latency.
+            * This only works for the `Madgwick` filter.
+            *
+            * Default: 0.4
+            */
+            beta: 0.4,
 
-          //   /*
-          //   * The filter noise values for the `Mahony` filter.
-          //   */
-          //   kp: 0.5, // Default: 0.5
-          //   ki: 0, // Default: 0.0
+            /*
+            * The filter noise values for the `Mahony` filter.
+            */
+            kp: 0.5, // Default: 0.5
+            ki: 0, // Default: 0.0
 
-          //   /*
-          //   * When the AHRS algorithm runs for the first time and this value is
-          //   * set to true, then initialisation is done.
-          //   *
-          //   * Default: false
-          //   */
-          //   doInitialisation: false,
-          // });
+            /*
+            * When the AHRS algorithm runs for the first time and this value is
+            * set to true, then initialisation is done.
+            *
+            * Default: false
+            */
+            doInitialisation: false,
+          });
 
-          // setMadgwick(madgwick)
+          setMadgwick(madgwick)
 
           const makeReadingHandler = (setData, acl: Gyroscope|Accelerometer|Magnetometer) => {
             return () => {
@@ -140,42 +140,39 @@ export const useAhrs = () => {
             }
           }
           
-          // const gAcl = new Gyroscope({ frequency: 10 })
+          const gAcl = new Gyroscope({ frequency: 10 })
           const aAcl = new Accelerometer({ frequency: 10 })
-          // const mAcl = new Magnetometer({ frequency: 10 })
+          const mAcl = new Magnetometer({ frequency: 10 })
 
-          // const gReadHandler = makeReadingHandler(setGData, gAcl)
+          const gReadHandler = makeReadingHandler(setGData, gAcl)
           const aReadHandler = makeReadingHandler(setAData, aAcl)
-          // const mReadHandler = makeReadingHandler(setMData, mAcl)
+          const mReadHandler = makeReadingHandler(setMData, mAcl)
           
-          // addEventListener("reading", gReadHandler);
-          aAcl.addEventListener("reading", () => {
-            console.log(aAcl)
-            setAData({ x: aAcl.x, y: aAcl.y, z: aAcl.z })
-          });
-          // addEventListener("reading", mReadHandler);
+          gAcl.addEventListener("reading", gReadHandler);
+          aAcl.addEventListener("reading", aReadHandler);
+          mAcl.addEventListener("reading", mReadHandler);
 
-          // gAcl.start()
+          gAcl.start()
           aAcl.start()
-          // mAcl.start()
+          mAcl.start()
           console.log("done init")
 
           return () => {
-            // removeEventListener("reading", gReadHandler);
-            removeEventListener("reading", aReadHandler);
-            // removeEventListener("reading", mReadHandler);
+            gAcl.removeEventListener("reading", gReadHandler);
+            aAcl.removeEventListener("reading", aReadHandler);
+            mAcl.removeEventListener("reading", mReadHandler);
           }
         }
       })
     }
   }, [])
 
-  // useEffect(() => {
-  //   console.log(gData)
-  //   if (!madgwick || gData.length == 0 || aData.length == 0) return
-  //   // madgwick.update(...gData, ...aData, ...mData)
-  //   // setEulerAngles(madgwick.getEulerAngles())
-  // }, [madgwick, gData, aData, mData])
+  useEffect(() => {
+    console.log(gData)
+    if (!madgwick || gData.length == 0 || aData.length == 0) return
+    madgwick.update(...gData, ...aData, ...mData)
+    setEulerAngles(madgwick.getEulerAngles())
+  }, [madgwick, gData, aData, mData])
 
-  return aData
+  return eulerAngles
 }
