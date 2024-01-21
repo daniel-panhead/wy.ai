dac1309fb544c8f2b0be6595868f5862e5442328
Reynella Liebig <reynella.liebig@gmail.com>
2024-01-20 20:02:07 -0800

    Add end-point

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from locator import xy_plane
+from locator import xy_plane as plane
 
 from fastapi import FastAPI
 
@@ -7,9 +7,16 @@ app = FastAPI()
 
 @app.get("/")
 def Home():
-    xy_plane.poop()
     return {"Home": "is where rey poops"}
 
 @app.get("/hello")
 def greetings():
     return {"Hello": "World"}
+
+
+@app.get("/coords")
+def coords():
+    lengthx = plane.q2_xaxis_length(1,2)
+    lengthy = plane.q1_yaxis_length(1,4)
+    return {"x" : plane.x_coord(lengthx), "y" : plane.y_coord(lengthy)}
+
