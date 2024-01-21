149af319ffccd72b197a067807e72d6f0206d2ec
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-01-21 11:23:46 -0800

    update mag less

--- frontend/src/util/sensors.ts
+++ frontend/src/util/sensors.ts
@@ -165,12 +165,12 @@ export const useAhrs = () => {
   }, [])
 
   useEffect(() => {
-    if (!madgwick || gData[0].length < 6 || aData[0].length < 6 || mData[0].length === 0) return
-
+    if (!madgwick || gData[0].length < 6 || aData[0].length < 6) return
     console.log(gData[0].length == aData[0].length, ' ', mData[0].length)
+
     const avgG = gData.map((axisArr) => axisArr.reduce((prev, curr) => prev += curr) / axisArr.length)
     const avgA = aData.map((axisArr) => axisArr.reduce((prev, curr) => prev += curr) / axisArr.length)
-    const avgM = mData.map((axisArr) => axisArr.reduce((prev, curr) => prev += curr) / axisArr.length)
+    const avgM = mData[0].length === 0 ? [] : mData.map((axisArr) => axisArr.reduce((prev, curr) => prev += curr) / axisArr.length)
     // @ts-expect-error it's fine
     madgwick.update(...avgG, ...avgA, ...avgM)
     setEulerAngles(madgwick.getEulerAngles())
