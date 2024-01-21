2b9e404b73951c59f60c924b046931a4601da53e
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-01-21 04:16:34 -0800

    test accel again

--- frontend/src/pages/CaptureCorner.tsx
+++ frontend/src/pages/CaptureCorner.tsx
@@ -15,10 +15,12 @@ const CaptureCorner = () => {
         const acl = new Accelerometer({ frequency: 60 });
 
         const handleReading = () => {
+          // @ts-expect-error go away man
           setAccel((prev) => acl)
-          console.log(`Acceleration along the X-axis ${acl.x}`);
-          console.log(`Acceleration along the Y-axis ${acl.y}`);
-          console.log(`Acceleration along the Z-axis ${acl.z}`);
+
+          // console.log(`Acceleration along the X-axis ${acl.x}`);
+          // console.log(`Acceleration along the Y-axis ${acl.y}`);
+          // console.log(`Acceleration along the Z-axis ${acl.z}`);
         }
 
         acl.addEventListener("reading", handleReading);
