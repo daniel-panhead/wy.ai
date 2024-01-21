141f713b3eb3a0624a1ece8a6764a4149e747551
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-01-20 18:51:58 -0800

    Reinit react native

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,9 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
+import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from 'react-native';
 
 export {
   // Catch any errors thrown by the Layout component.
