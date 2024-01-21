2a0ae172b7a7d8cdecd8d66579ca905be99351bb
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-01-21 11:31:15 -0800

    simple degree diff

--- frontend/src/pages/Guiding.tsx
+++ frontend/src/pages/Guiding.tsx
@@ -9,6 +9,9 @@ const Guiding = () => {
   const ratio = isLandscape ? size.width / size.height : size.height /
     size.width;
 
+  const targetDeg = 48;
+  const diff = ahrs.heading - targetDeg
+
   return (
     <div className="w-full h-full bg-black">
       <div className="absolute z-10 w-full p-6 opacity-60">
@@ -24,7 +27,7 @@ const Guiding = () => {
       <div className="absolute w-full bottom-14">
         <div className="flex flex-col justify-center w-full px-8 text-light-light-green">
           <span className="text-4xl font-semibold">10 FT AWAY</span>
-          <span className="text-xl font-medium">WALK STRAIGHT</span>
+          <span className="text-xl font-medium">Turn {diff} right</span>
         </div>
       </div>
       <div className="absolute left-[50%] -ml-3 top-[50%] -mt-3 bg-white rounded-full w-6 h-6 opacity-40 animate-pulse"></div>
